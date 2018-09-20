6aac6aaaf2eacd083b29388861a2b7d7bdc59262
CAHI2 <caleb.hill@hotmail.co.uk>
2018-09-20 13:13:50 +0100

    Adding in code to place a slideshow

--- js/website.js
+++ js/website.js
@@ -1,1 +1,17 @@
- 
+var SlideIndex = 1;
+showDivs(SlideIndex);
+
+function plusDivs(n) {
+  showDivs(SlideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideshow");
+  if (n > x.length){SlideIndex = 1}
+  if (n < 1) {SlideIndex = x.length}
+  for (i = 0; x.length; i++){
+    x[i].style.display = "none";
+  }
+  x[SlideIndex-1].style.display = "block";
+}
