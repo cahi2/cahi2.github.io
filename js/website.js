92d3f027c08b1ecde4e1d17816e33cbd2ac02002
CAHI2 <caleb.hill@hotmail.co.uk>
2018-09-20 14:58:17 +0100

    Changed a bunch of stuff - moved scripts around lol jkjk

--- js/website.js
+++ js/website.js
@@ -1,17 +1,17 @@
-var SlideIndex = 1;
-showDivs(SlideIndex);
+var slideIndex = 1;
+showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(SlideIndex += n);
+  showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slideshow");
-  if (n > x.length){SlideIndex = 1}
-  if (n < 1) {SlideIndex = x.length}
-  for (i = 0; x.length; i++){
+  if (n > x.length){slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++){
     x[i].style.display = "none";
   }
-  x[SlideIndex-1].style.display = "block";
+  x[slideIndex-1].style.display = "block";
 }
