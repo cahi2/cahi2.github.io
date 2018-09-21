231b7d9a4c1dfd81b9f3884ebb205f8164b6fbd8
CAHI2 <caleb.hill@hotmail.co.uk>
2018-09-21 09:41:37 +0100

    Added a javascript for animated progress bar, also added some comments into Index.html

--- js/website.js
+++ js/website.js
@@ -15,3 +15,18 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
