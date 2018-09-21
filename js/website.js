8a0407bcef2fa93d744f30f3dec170fb491f2f42
CAHI2 <caleb.hill@hotmail.co.uk>
2018-09-21 10:21:49 +0100

    Added a completely useles progress bar racer. Script is in website.js

--- js/website.js
+++ js/website.js
@@ -1,3 +1,5 @@
+//Script for the slideshow------------------------------------------------------
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -16,17 +18,34 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";
 }
 
+//Script for the progress bar racer---------------------------------------------
 
 function move() {
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++;
-            elem.style.width = width + '%';
-        }
+  var racer1 = document.getElementById("racer1");
+  var racer2 = document.getElementById("racer2");
+  var racer3 = document.getElementById("racer3");
+  var racer4 = document.getElementById("racer4");
+
+  var width1 = 1;
+  var width2 = 1;
+  var width3 = 1;
+  var width4 = 1;
+
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (width1 >= 100 || width2 >= 100 || width3 >= 100 || width4 >= 100) {
+      clearInterval(id);
+    }else {
+      width1 = width1 + Math.random();
+      width2 = width2 + Math.random();
+      width3 = width3 + Math.random();
+      width4 = width4 + Math.random();
+
+      racer1.style.width = width1 + '%';
+      racer2.style.width = width2 + '%';
+      racer3.style.width = width3 + '%';
+      racer4.style.width = width4 + '%';
+
     }
+  }
 }
