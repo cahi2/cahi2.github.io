3ee6df0c4cd4bb9cf0041bd3bb6c0bc398d43690
CAHI2 <caleb.hill@hotmail.co.uk>
2018-09-21 13:35:31 +0100

    The progress bars now take up less line space in website.js

--- js/website.js
+++ js/website.js
@@ -18,34 +18,28 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";
 }
 
-//Script for the progress bar racer---------------------------------------------
+//Script for the progress bar racer----------------------------------------------
 
 function move() {
-  var racer1 = document.getElementById("racer1");
-  var racer2 = document.getElementById("racer2");
-  var racer3 = document.getElementById("racer3");
-  var racer4 = document.getElementById("racer4");
-
-  var width1 = 1;
-  var width2 = 1;
-  var width3 = 1;
-  var width4 = 1;
-
+  var bars = [];
+  var width = [];
+  //Loop which declares the variables of the progress bars
+  for (i=0; i<4; i++){
+    bars[i] = document.getElementById("racer"  + i);
+    width[i] = 1;
+  }
   var id = setInterval(frame, 50);
+
   function frame() {
-    if (width1 >= 100 || width2 >= 100 || width3 >= 100 || width4 >= 100) {
-      clearInterval(id);
-    }else {
-      width1 = width1 + Math.random();
-      width2 = width2 + Math.random();
-      width3 = width3 + Math.random();
-      width4 = width4 + Math.random();
-
-      racer1.style.width = width1 + '%';
-      racer2.style.width = width2 + '%';
-      racer3.style.width = width3 + '%';
-      racer4.style.width = width4 + '%';
-
+    for (i = 0; i <4; i++){
+      if (width[i] >=100) {
+        clearInterval(id);
+      } else {
+        width[i] = width[i] + Math.round(Math.random());
+        bars[i].style.width = width[i] + '%';
+      }
     }
+
+
   }
 }
