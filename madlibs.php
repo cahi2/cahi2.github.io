4804824a40c062b50a323a265016add65b6af82b
CAHI2 <caleb.hill@hotmail.co.uk>
2018-09-25 12:18:56 +0100

    removed scripts from this php file

--- madlibs.php
+++ madlibs.php
@@ -3,10 +3,6 @@
 <head>
   <title>Code Week Site</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- SCRIPTS -->
-  <script src="js/madlibs.js"></script>
-
   <!-- STYLESHEETS -->
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="css/website.css">
