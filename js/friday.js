06a15d082aa17ca50c8f8f23a9f5a0ebb81807d2
CAHI2 <caleb.hill@hotmail.co.uk>
2018-09-26 11:43:45 +0100

    Bosh

--- js/friday.js
+++ js/friday.js
@@ -5,9 +5,6 @@ var slideIndex = 1;
 function showImage(n) {
   var i;
   var x = document.getElementsByClassName("fridayImages");
-  for (i = 0; i < x.length; i++){
-    x[i].style.display = "none";
-  }
 
-  x[n].style.display = "block";
+  x[n].style.display = "none";
 }
