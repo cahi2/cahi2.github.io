51ad0f0e416d54215fed31fefac2a8bfc8d1dbdf
CAHI2 <caleb.hill@hotmail.co.uk>
2018-10-02 13:34:13 +0100

    Boom madlibs

--- js/madlibs.js
+++ js/madlibs.js
@@ -1,4 +1,12 @@
 function confirmInput() {
    fname = document.forms[0].fname.value;
-   alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
+   fadj = document.forms[1].fadj.value;
+   fplace = document.forms[2].fplace.value;
+   ffruit = document.forms[3].ffruit.value;
+   focc = document.forms[4].focc.value;
+
+   alert("There once was a  " + focc + " called " + fname + ". They'd often be called " + fadj
+   + " by the locals because they smelled like " + ffruit + "!");
+
+
 }
